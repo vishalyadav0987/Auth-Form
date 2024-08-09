cf12a4c6546ee03d549c3dace6a6719e11eb57c6
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-08-09 21:14:48 +0530

    ' Added Reset Password Route
    ' Route Definition: Created a POST route at /reset-password/:token to handle password reset requests.
    
    ' Controller Function: The resetPassword function is executed when a request is made to this route with a valid token.
    
    ' Module Export: This route allows users to reset their password by providing a valid reset token.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,6 +5,7 @@ const {
     signIn,
     logout,
     forgetPassword,
+    resetPassword,
 } = require('../controllers/authController');
 const router = express.Router();
 
@@ -17,6 +18,7 @@ router.route('/logout').post(logout); // logout
 
 router.route('/verify-email').post(verifyEmail);
 router.route("/forget-password").post(forgetPassword);
+router.route("/reset-password/:token").post(resetPassword);
 
 
 module.exports = router;
