bdd395ce0d7c1db7d4cf62573807e559e95f91c2
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-08-09 23:21:28 +0530

    ' Commit Description: ' ' Set up main routing for the application. ' - Configured routes for user authentication and password management pages. ' - Integrated Chakra UI Container for consistent layout. ' - Added routes for RegisterPage, VerifyEmail, LoginPage, ForgotPage, and ResetPassword components.
    ' Summary:
    ' Implemented routing for various authentication and password-related pages in the React application.
    ' Used Chakra UI's Container component for overall layout management.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
+import { Container } from '@chakra-ui/react'
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from './Pages/LoginPage/LoginPage'
+import RegisterPage from './Pages/RegisterPage/RegisterPage'
+import ForgotPage from './Pages/ForgetPage/ForgetPage'
+import VerifyEmail from './Pages/VerifyEmail/VerifyEmail'
+import ResetPassword from './Pages/ResetPassword/ResetPassword'
 
 const App = () => {
   return (
-    <div>
-      This Auth form
-    </div>
+    <Container>
+      <Routes>
+        <Route path='/sign-up' Component={RegisterPage} /> {/*register*/}
+        <Route path='/verify-email' Component={VerifyEmail} />
+        <Route path='/sign-in' Component={LoginPage} /> {/*login*/}
+        <Route path='/forget-password' Component={ForgotPage} />
+        <Route path='/reset-password' Component={ResetPassword} />
+      </Routes>
+    </Container>
   )
 }
 
