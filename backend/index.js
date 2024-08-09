00f35f5b69b5ef9a86fdbd1c5c53084852cca2d5
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-08-09 13:46:01 +0530

    ### Added Basic Express Server Setup with MongoDB Connection
    - **Environment Configuration:** Loaded environment variables using `dotenv` and set the server port from `process.env.PORT`, defaulting to 3000 if not specified.
    - **Middleware:** Configured Express to parse JSON requests and handle cookies with `cookie-parser`.
    - **Database Connection:** Imported `connectDB` utility to connect to MongoDB using the URI provided in `process.env.MONGO_URI`.
    - **Test Route:** Added a `/test` route to verify server functionality with a simple response.
    - **Server Initialization:** Wrapped server startup in an asynchronous `start` function that attempts to connect to the database before listening on the specified port.

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+const cookieParser = require('cookie-parser');
+const connectDB = require("./connectDB/connect");
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+
+app.use("/test", (req, res) => {
+    res.send("This route using for testing purpose. Yaah it's working");
+})
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () => {
+            console.log(`Server running at http://localhost:${PORT}`);
+        })
+    } catch (error) {
+        console.log("Somethig went wrong in connecting to the server")
+    }
+}
+
+start();
