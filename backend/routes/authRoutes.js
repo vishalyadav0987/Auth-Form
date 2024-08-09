c606d482bc69c776d229a276a88e0658bed8f1df
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-08-09 19:47:06 +0530

    ' Added Logout Route for User Session Termination
    ' Route Definition: Created a POST route at /logout to handle user logout requests.
    
    ' Controller Function: The logout function is executed when a request is made to this route.
    
    ' Module Export: This route is used to terminate the user session by clearing authentication cookies.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ const {
     signUp,
     verifyEmail,
     signIn,
+    logout,
 } = require('../controllers/authController');
 const router = express.Router();
 
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.route('/sign-up').post(signUp); // register
 router.route('/sign-in').post(signIn); // login
+router.route('/logout').post(logout); // logout
 
 
 router.route('/verify-email').post(verifyEmail);
