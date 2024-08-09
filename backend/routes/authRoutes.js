a15aab9dbf1ecd5c6e67100b24432b4ae5239a33
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-08-09 16:03:11 +0530

    ' Added Email Verification Route
    ' Route Definition: Created a POST route at /verify-email to handle email verification requests.
    
    ' Controller Function: The verifyEmail function is executed when a request is made to this route.
    
    ' Module Export: This route can be used in the application to manage email verification processes.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const {
     signUp,
+    verifyEmail,
 } = require('../controllers/authController');
 const router = express.Router();
 
 
 
 router.route('/sign-up').post(signUp); // register
+
+
+router.route('/verify-email').post(verifyEmail);
 
 
 module.exports = router;
