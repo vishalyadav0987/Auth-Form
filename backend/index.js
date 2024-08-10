6c78fc22e376d64d4307facae5454506c332dc55
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-08-10 23:01:10 +0530

    Commit description: - Deployment TRY 1

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 require('dotenv').config();
-const connectDB = require("./connectDB/connect");
 const cookieParser = require('cookie-parser');
+const path = require('path')
+const connectDB = require("./connectDB/connect");
 const authRoutes = require('./routes/authRoutes');
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
+
+
+
+if (process.env.NODE_ENV === "production") {
+    // app.use(express.static(path.join(__dirname,"frontend/build")))
+
+    const frontendPath = path.join(__dirname, '..', 'frontend', 'dist');
+    app.use(express.static(frontendPath));
+
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(frontendPath, "index.html"))
+    })
+}
 
 app.use("/test", (req, res) => {
     res.send("This route is used for testing purposes. Yeah, it's working!");
