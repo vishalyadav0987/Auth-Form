b1db1e4959918925655f4add8733d4cc771cb345
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-08-10 18:09:14 +0530

    Commit description: ' Import necessary modules ' Imports React hooks for state management and routing. ' Imports custom components and utilities for handling authentication and notifications. ' ' The code defines a React functional component that handles password reset functionality. ' ' Key Features: ' - **State Management**: Uses `useState` to manage password, confirm password, and visibility of the password field. ' - **Routing**: Uses `useNavigate` and `useParams` for routing and accessing URL parameters. ' - **API Request**: Sends a POST request to reset the user's password using a token from the URL. ' - **Password Validation**: Checks if the passwords match before sending the request. ' - **Notifications**: Uses `react-hot-toast` to display success or error messages based on the response. ' ' The `handleResetPassword` function: ' - Prevents default form submission behavior. ' - Sets loading state to true. ' - Validates if the password and confirm password fields match. ' - Sends a POST request to `/api/v1/auth/reset-password/${token}` with the new password. ' - Displays a success message and navigates to the sign-in page after a delay if the request is successful. ' - Displays an error message if the request fails. ' - Resets loading state to false after handling the request. ' ' The component uses Chakra UI icons to toggle password visibility and a custom `PasswordStrengthMeter` component. ' It handles the entire password reset process including user feedback and navigation.

--- frontend/src/Pages/ResetPassword/ResetPassword.jsx
+++ frontend/src/Pages/ResetPassword/ResetPassword.jsx
@@ -5,13 +5,58 @@ import {
   Flex,
   FormControl,
   FormLabel,
+  InputGroup,
+  InputRightElement,
   Heading,
   Input,
   Stack,
   useColorModeValue,
 } from '@chakra-ui/react'
+import { useNavigate, useParams } from "react-router-dom";
+import { useAuthContext } from '../../Context/AuthContext';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { useState } from 'react';
+import PasswordStrengthMeter from '../../Component/PasswordStrengthMeter/PasswordStrengthMeter'
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 
 export default function ResetPassword() {
+  const [showPassword, setShowPassword] = useState(false);
+  const { token } = useParams();
+  const navigate = useNavigate();
+  const { loading, setLoading } = useAuthContext();
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return;
+    }
+    try {
+      const response = await axios.post(
+        `/api/v1/auth/reset-password/${token}`,
+        { password },
+        { headers: { "Content-Type": "application/json" } }
+      );
+      if (response.data.success) {
+        toast.success(response.data.message);
+        setTimeout(() => {
+          navigate("/sign-in");
+        }, 2000);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log("Error in handleForgetPassword ->", error)
+      toast.error(error.message);
+    }
+    finally {
+      setLoading(false);
+    }
+  }
   return (
     <Flex
       minH={'100vh'}
@@ -30,20 +75,56 @@ export default function ResetPassword() {
         <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
           Enter new password
         </Heading>
-        <FormControl id="password" isRequired>
-          <FormLabel>Password</FormLabel>
-          <Input type="password" />
-        </FormControl>
-        <Stack spacing={6}>
-          <Button
-            bg={'blue.400'}
-            color={'white'}
-            _hover={{
-              bg: 'blue.500',
-            }}>
-            Submit
-          </Button>
-        </Stack>
+        <form onSubmit={handleResetPassword}>
+          <FormControl id="password" isRequired>
+            <FormLabel>New Password</FormLabel>
+            <InputGroup>
+              <Input type={showPassword ? 'text' : 'password'}
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+              <InputRightElement h={'full'}>
+                <Button
+                  variant={'ghost'}
+                  onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                  {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
+          <FormControl id="password" isRequired>
+            <FormLabel>Confirm Password</FormLabel>
+            <InputGroup>
+              <Input type={showPassword ? 'text' : 'password'}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={confirmPassword}
+              />
+              <InputRightElement h={'full'}>
+                <Button
+                  variant={'ghost'}
+                  onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                  {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
+
+          <PasswordStrengthMeter password={password} />
+          <Stack spacing={6}>
+            <Button
+              mt={4}
+              type='submit'
+              isLoading={loading}
+              loadingText="Reseting"
+              bg={'blue.400'}
+              color={'white'}
+              _hover={{
+                bg: 'blue.500',
+              }}>
+              Submit
+            </Button>
+          </Stack>
+        </form>
       </Stack>
     </Flex>
   )
