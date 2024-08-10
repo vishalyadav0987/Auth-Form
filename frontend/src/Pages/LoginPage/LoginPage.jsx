9afde48eb8c0774ea89827f004ea42baf41c84a8
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-08-10 18:06:24 +0530

    ' Commit Description: ' ' Added login functionality with error handling and user feedback. ' ' - Login Functionality: '   - Added `loginSubmitHandle` function to handle user login: '     - Sends a POST request to `/api/v1/auth/sign-in` with user email and password. '     - On successful login, updates authentication state, displays a success message, and sets user data. '     - On failure, updates authentication state to false, displays an error message, and clears user data. '   - Used `setLoading` to show a loading state while the request is being processed. '   - Integrated `toast` for displaying success and error messages based on the response. '   - Included error handling to log errors and provide feedback. ' ' This functionality ensures secure user login with appropriate feedback and state management.

--- frontend/src/Pages/LoginPage/LoginPage.jsx
+++ frontend/src/Pages/LoginPage/LoginPage.jsx
@@ -7,7 +7,6 @@ import {
     FormLabel,
     Input,
     InputGroup,
-    HStack,
     InputRightElement,
     Stack,
     Button,
@@ -18,9 +17,43 @@ import {
 import { useState } from 'react'
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
 import { Link } from 'react-router-dom'
+import { useAuthContext } from '../../Context/AuthContext'
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 export default function LoginPage() {
-    const [showPassword, setShowPassword] = useState(false)
+    const [showPassword, setShowPassword] = useState(false);
+    const { loading, setLoading, setAuthUser, setIsAuthenticate } = useAuthContext();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const loginSubmitHandle = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            const response = await axios.post(
+                '/api/v1/auth/sign-in',
+                { email, password },
+                { headers: { "Content-Type": "application/json" } }
+            );
+            if (response.data.success) {
+                setIsAuthenticate(true);
+                toast.success(response.data.message);
+                setAuthUser(response.data.data);
+            } else {
+                setIsAuthenticate(false);
+                toast.error(response.data.message);
+            }
+        } catch (error) {
+            setIsAuthenticate(false);
+            console.log("Error in loginSubmitHandle->", error)
+            toast.error(error.message);
+            setAuthUser(null);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
 
     return (
         <Flex
@@ -43,52 +76,62 @@ export default function LoginPage() {
                         sm: "400px",
                     }}
                 >
-                    <Stack spacing={4}>
-                        <FormControl id="email" isRequired>
-                            <FormLabel>Email address</FormLabel>
-                            <Input type="email" />
-                        </FormControl>
-                        <FormControl id="password" isRequired>
-                            <FormLabel>Password</FormLabel>
-                            <InputGroup>
-                                <Input type={showPassword ? 'text' : 'password'} />
-                                <InputRightElement h={'full'}>
-                                    <Button
-                                        variant={'ghost'}
-                                        onClick={() => setShowPassword((showPassword) => !showPassword)}>
-                                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                                    </Button>
-                                </InputRightElement>
-                            </InputGroup>
-                        </FormControl>
-                        <Stack
-                            width={"100%"}>
-                            <Link to={"/forget-password"}>
-                                <Text color={'blue.500'}
-                                    textAlign={"end"} px={1}
-                                    cursor={"pointer"}>Forgot password?</Text>
-                            </Link>
+                    <form onSubmit={loginSubmitHandle}>
+                        <Stack spacing={4}>
+                            <FormControl id="email" isRequired>
+                                <FormLabel>Email address</FormLabel>
+                                <Input type="email"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    value={email}
+                                />
+                            </FormControl>
+                            <FormControl id="password" isRequired>
+                                <FormLabel>Password</FormLabel>
+                                <InputGroup>
+                                    <Input type={showPassword ? 'text' : 'password'}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        value={password}
+                                    />
+                                    <InputRightElement h={'full'}>
+                                        <Button
+                                            variant={'ghost'}
+                                            onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                                            {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                                        </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                            </FormControl>
+                            <Stack
+                                width={"100%"}>
+                                <Link to={"/forget-password"}>
+                                    <Text color={'blue.500'}
+                                        textAlign={"end"} px={1}
+                                        cursor={"pointer"}>Forgot password?</Text>
+                                </Link>
+                            </Stack>
+                            <Stack spacing={10} pt={2}>
+                                <Button
+                                    type='submit'
+                                    isLoading={loading}
+                                    loadingText="Submitting"
+                                    size="lg"
+                                    bg={'blue.400'}
+                                    color={'white'}
+                                    _hover={{
+                                        bg: 'blue.500',
+                                    }}>
+                                    login
+                                </Button>
+                            </Stack>
+                            <Stack pt={6}>
+                                <Text align={'center'}>
+                                    Don't have an account <Link to="/sign-up" style={{
+                                        color: "#179cf0"
+                                    }}>Sign Up</Link>
+                                </Text>
+                            </Stack>
                         </Stack>
-                        <Stack spacing={10} pt={2}>
-                            <Button
-                                loadingText="Submitting"
-                                size="lg"
-                                bg={'blue.400'}
-                                color={'white'}
-                                _hover={{
-                                    bg: 'blue.500',
-                                }}>
-                                Sign up
-                            </Button>
-                        </Stack>
-                        <Stack pt={6}>
-                            <Text align={'center'}>
-                                Don't have an account <Link to="/sign-up" style={{
-                                    color: "#179cf0"
-                                }}>Sign Up</Link>
-                            </Text>
-                        </Stack>
-                    </Stack>
+                    </form>
                 </Box>
             </Stack>
         </Flex>
