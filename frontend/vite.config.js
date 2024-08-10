8a1b6e30ab89981b1645bde3b8ce4a6c7c7b6d6e
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-08-10 17:50:48 +0530

    ' Commit Description: ' ' Configured Vite for the React project by setting up a custom server port and proxy settings. ' ' - Integrated React with Vite: Added the `@vitejs/plugin-react` plugin to enable React support in the Vite build process. ' - Custom Server Port: Configured the development server to run on port 4000 instead of the default port, which might be used to avoid conflicts with other local services. ' - API Proxy Setup: Set up a proxy for API requests starting with `/api/v1` to be forwarded to `http://localhost:3000`. '   This configuration is useful for development purposes, enabling the frontend to communicate with the backend seamlessly without CORS issues.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4000,
+    proxy: {
+      '/api/v1': {
+        target: "http://localhost:3000"
+      },
+    },
+  },
 })
