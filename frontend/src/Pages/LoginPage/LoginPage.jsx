6c74961744fa359e800c486c1ab01f8b3ac4f63e
vishal <vishal.yadav@myfindoc.com>
2026-05-30 11:41:23 +0530

    Add dockerignore file

--- frontend/src/Pages/LoginPage/LoginPage.jsx
+++ frontend/src/Pages/LoginPage/LoginPage.jsx
@@ -63,7 +63,7 @@ export default function LoginPage() {
             <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
                 <Stack align={'center'}>
                     <Heading fontSize={'4xl'} textAlign={'center'}>
-                        Login another another
+                        Login another
                     </Heading>
                 </Stack>
                 <Box
