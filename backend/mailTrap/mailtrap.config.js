d09aefa69bc76275d8e6835af216364ba9857fe0
vishal <vishal.yadav@myfindoc.com>
2026-06-01 12:30:22 +0530

    update mailtrap token in docker-compose.yaml

--- backend/mailTrap/mailtrap.config.js
+++ backend/mailTrap/mailtrap.config.js
@@ -2,7 +2,6 @@ const { MailtrapClient } = require('mailtrap');
 require("dotenv").config();
 
 const mailtrapClient = new MailtrapClient({
-    endpoint: process.env.MAILTRAP_ENDPOINT,
     token: process.env.MAILTRAP_TOKEN,
 });
 
